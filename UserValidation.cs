03a4900cc707e913a441a2a8ab23096b5b742422
poojakhandale <poojakhandale2000@gmail.com>
2023-03-02 20:49:48 +0530

    Validate the E-mail

--- UserValidation.cs
+++ UserValidation.cs
@@ -14,11 +14,20 @@ namespace RegularExpression
         public class User
         {
 
-            public Boolean ValidateFirstName(string firstName)
+
+            public Boolean ValidateName(string firstName)
             {
                 String Pattern = "[A-Z]{1}[a-z]{2,}";
                 return Regex.IsMatch(firstName, Pattern);
             }
+
+            public Boolean Validatemail(string email)
+            {
+                string Pattern = "^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*.[a-zA-Z]{2,}$";
+                return Regex.IsMatch(email, Pattern);
+            }
+
         }
     }
-}
+    }
+
