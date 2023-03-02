c845602807666db41aca0704fbe6cfe811abc74d
poojakhandale <poojakhandale2000@gmail.com>
2023-03-02 20:00:04 +0530

    validate first name

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static RegularExpression.UserValidation;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        User userinfo = new User();
+        Boolean Firstname = userinfo.ValidateFirstName("Kratos");
+        if (Firstname == false)
+        {
+            Console.WriteLine("Enter the first name with capitalze and atleast had a 3 character");
+        }
+    }
+}
