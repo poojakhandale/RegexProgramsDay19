edccd6bf22f93eee8a7399f455697c0c41a5c61e
poojakhandale <poojakhandale2000@gmail.com>
2023-03-02 19:58:18 +0530

    Validate first name

---
+++ UserValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class UserValidation
+    {
+
+
+        public class User
+        {
+
+            public Boolean ValidateFirstName(string firstName)
+            {
+                String Pattern = "[A-Z]{1}[a-z]{2,}";
+                return Regex.IsMatch(firstName, Pattern);
+            }
+        }
+    }
+}
